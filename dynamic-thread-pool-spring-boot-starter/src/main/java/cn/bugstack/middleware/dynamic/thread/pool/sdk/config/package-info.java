ea99a645789d1d46a5bf0d9144236331aa78dd2a
rmxlll <renmengxi@wust.edu.cn>
2024-09-12 00:39:49 +0800

    动态线程池组件

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/cn/bugstack/middleware/dynamic/thread/pool/sdk/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @description auto config 配置入口
+ */
+package cn.bugstack.middleware.dynamic.thread.pool.sdk.config;
---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/cn/bugstack/middleware/dynamic/thread/pool/sdk/trigger/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @description 触发器 job、listener
+ */
+package cn.bugstack.middleware.dynamic.thread.pool.sdk.trigger;
